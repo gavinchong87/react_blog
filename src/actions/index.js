502bc70be6d8f7e689dc97a9d6f4231886a86350
Luna Wang <luna@lunas-mbp-2.lan>
2020-12-22 22:39:58 -0500

    second memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,18 +7,18 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
-// export const fetchUser = id => dispatch => {
-//     _fetchUser(id, dispatch);
-// };
-
-// const _fetchUser =  _.memoize(async (id, dispatch) => {
-//     const response = await jsonPlaceHolder.get(`/users/${id}`);
-
-//     dispatch({ type: 'FETCH_USER', payload: response.data });
-// });
-
-export const fetchUser = () => async dispatch => {
-    const response = await jsonPlaceHolder.get('/users/${id}');
-
-    dispatch({ type: 'FETCH_USER', payload: response.data })
+export const fetchUser = id => dispatch => {
+    _fetchUser(id, dispatch);
 };
+
+const _fetchUser =  _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+});
+
+// export const fetchUser = () => async dispatch => {
+//     const response = await jsonPlaceHolder.get('/users/${id}');
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data })
+// };
